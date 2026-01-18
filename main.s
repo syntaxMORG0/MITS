c3b0dc81a3d35f510b424c11aa4e9ea03118d4fe
syntaxMORG0 <129966083+syntaxMORG0@users.noreply.github.com>
2026-01-18 14:49:44 +0100

    fixed content in main.s placeholder

--- main.s
+++ main.s
@@ -1,22 +1,4 @@
 _start:
-	char prm, "Anngi navnet ditt!"
-	mov msg, hex=prm
-	mov prm, b31 msg
-
-	vga prm
-	mov prm, 0
-	mov msg, 0
-
-	rdl nam
-	mov nbm, hex=nam
-
-	char msg, "Velkommen "
-	mov amg, hex=msg
-
-	addr rax, amg + nbm
-
-	mov acm, b31 rax
-	vga acm
 	
 	mov ext, code=0
 	exec ext
